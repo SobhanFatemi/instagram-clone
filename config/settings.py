8c8496e18e870d79559597425efc695b1e237469
Sobhan Fatemi Devin <sobhanfatemi55@gmail.com>
2026-06-14 21:45:44 +0330

    Removed ERD.md

--- config/settings.py
+++ config/settings.py
@@ -158,7 +158,7 @@ SPECTACULAR_SETTINGS = {
 
 # JWT
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=3000000),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=14),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
